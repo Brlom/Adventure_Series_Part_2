7fa77771132110b77c3e0a971b495835cac88133
brlom <brlom007@gmail.com>
2018-10-11 13:01:55 +0100

    start on adventureGame 2.0

--- entities/game.js
+++ entities/game.js
@@ -2,10 +2,30 @@ const goToNextRoom = require("../inquirer");
 
 const Game = function(gameName) {
   this.gameName = gameName;
+  this.playerName = 'default';
+  this.playerFear = 0;
+  this.tooScared = false;
 };
 
 Game.prototype.startGame = function() {
   goToNextRoom(this);
+};
+
+Game.prototype.changePlayerName = function(name) {
+  this.playerName = name;
+};
+
+Game.prototype.changePlayerFear = function(amount) {
+  if (this.playerFear === 0 && amount < 0) return 0;   
+  this.playerFear += amount;
+};
+
+Game.prototype.checkIfTooScared = function(num) {
+  if (this.playerFear >= num) {
+    this.tooScared = true;
+  } else {
+    this.tooScared = false;
+  };
 };
 
 module.exports = Game;
--- spec/game.spec.js
+++ spec/game.spec.js
@@ -2,8 +2,42 @@ const { expect } = require("chai");
 const Game = require("../entities/game");
 
 describe("GAME system", () => {
-  it("creates an new Game object with the correct name", () => {
-    // create your new Game
-    // expect it to have...
+  describe("game", () => {
+    it("creates an new Game object with the correct name", () => {
+      const newGame = new Game("The Haunt");
+      expect(newGame.gameName).to.equal("The Haunt");
+    });
+    it("creates an new player object set to default", () => {
+      const newGame = new Game("The Haunt");
+      expect(newGame.playerName).to.equal("default");
+    });
+    it("creates an changePlayer method that updates existing playerName", () => {
+      const newGame = new Game("The Haunt");
+      newGame.changePlayerName("Charlotte");
+      expect(newGame.playerName).to.equal("Charlotte");
+    });
+    it("creates a playerFear object set to 0", () => {
+      const newGame = new Game("The Haunt");
+      expect(newGame.playerFear).to.equal(0);
+    });
+    it("creates a changePlayerFear method that updates existing playerFear", () => {
+      const newGame = new Game("The Haunt");
+      newGame.changePlayerFear(4);
+      expect(newGame.playerFear).to.equal(4);
+      newGame.changePlayerFear(-2);
+      expect(newGame.playerFear).to.equal(2);
+      newGame.changePlayerFear(-2);
+      expect(newGame.playerFear).to.equal(0);
+      newGame.changePlayerFear(-1);
+      expect(newGame.playerFear).to.equal(0);
+    });
+    it("checks if playerFear is too high, and returns boolean", () => {
+      const newGame = new Game("The Haunt");
+      newGame.checkIfTooScared(100);
+      expect(newGame.tooScared).to.equal(false);
+      // newGame.changePlayerFear(101);
+      // expect(newGame.playerFear).to.equal(101);
+      // expect(newGame.tooScared).to.equal(true);
+    });
   });
 });
